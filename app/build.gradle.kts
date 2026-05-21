4d72c0761eb6fe11f448f446104636fc7fdc272c
dako-disturb0 <dako.disturb@gmail.com>
2026-05-22 01:06:00 +0700

    build: bump version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.peerx.p2p"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     ndk {
